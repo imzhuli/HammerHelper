301fee446fade1113a412b680d21cd3ec175d26b
xelee <imzhuli@vip.qq.com>
2023-10-17 19:06:47 +0800

    card defines

--- Hammer/apps/cards/main.cpp
+++ Hammer/apps/cards/main.cpp
@@ -6,17 +6,13 @@ int main(int, char *[]) {
     cout << "hello cards" << endl;
     cout << "total faces: " << Length(CardFace) << endl;
 
-    // size_t Total = 0;
-    // for(size_t i = 0; i < Length(CardColor); ++i) {
-    //     for(size_t j = 0; j < Length(CardFace); ++j) {
-    //         auto C = xCard{
-    //             uint8_t(Total++),
-    //             uint8_t(j),
-    //             uint8_t(i),
-    //         };
-    //         cout << C.ToString() << endl;
-    //     }
-    // }
+    for(auto C : BaseCards) {
+        auto ReverseCheck = xCard::GetIndex(C.Face, C.Color);
+        cerr << C.ToString() << " ReverseCheck: " << ReverseCheck << endl;
+        if(ReverseCheck != C.Index) {
+            Fatal();
+        }
+    }
 
     return 0;
 }
